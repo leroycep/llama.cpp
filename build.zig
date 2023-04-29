7e9a144de2cf111888ae8ebe74a7d21b0400bac5
LeRoyce Pearson <contact@leroycepearson.dev>
2023-04-29 00:43:15 -0600

    Update build.zig to zig 0.11.0-dev.2868+1a455b2dd

--- build.zig
+++ build.zig
@@ -2,23 +2,25 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
     const want_lto = b.option(bool, "lto", "Want -fLTO");
 
-    const lib = b.addStaticLibrary("llama", null);
+    const lib = b.addStaticLibrary(.{
+        .name = "llama",
+        .target = target,
+        .optimize = optimize,
+    });
     lib.want_lto = want_lto;
-    lib.setTarget(target);
-    lib.setBuildMode(optimize);
     lib.linkLibCpp();
     lib.addIncludePath(".");
     lib.addIncludePath("examples");
     lib.addCSourceFiles(&.{
         "ggml.c",
-    }, &.{"-std=c11"});
+    }, &.{});
     lib.addCSourceFiles(&.{
         "llama.cpp",
     }, &.{"-std=c++11"});
-    lib.install();
+    b.installArtifact(lib);
 
     const build_args = .{ .b = b, .lib = lib, .target = target, .optimize = optimize, .want_lto = want_lto };
 
@@ -29,7 +31,7 @@ pub fn build(b: *std.build.Builder) void {
 
     // create "zig build run" command for ./main
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
@@ -39,23 +41,25 @@ pub fn build(b: *std.build.Builder) void {
     run_step.dependOn(&run_cmd.step);
 }
 
-fn build_example(comptime name: []const u8, args: anytype) *std.build.LibExeObjStep {
+fn build_example(name: []const u8, args: anytype) *std.build.LibExeObjStep {
     const b = args.b;
     const lib = args.lib;
     const want_lto = args.want_lto;
 
-    const exe = b.addExecutable(name, null);
+    const exe = b.addExecutable(.{
+        .name = name,
+        .target = args.target,
+        .optimize = args.optimize,
+    });
     exe.want_lto = want_lto;
-    lib.setTarget(args.target);
-    lib.setBuildMode(args.optimize);
     exe.addIncludePath(".");
     exe.addIncludePath("examples");
     exe.addCSourceFiles(&.{
-        std.fmt.comptimePrint("examples/{s}/{s}.cpp", .{name, name}),
+        b.fmt("examples/{s}/{s}.cpp", .{ name, name }),
         "examples/common.cpp",
     }, &.{"-std=c++11"});
     exe.linkLibrary(lib);
-    exe.install();
+    b.installArtifact(exe);
 
     return exe;
 }
