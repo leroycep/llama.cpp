ac4b918b48c3b468b87c309770194c91c833787b
LeRoyce Pearson <contact@leroycepearson.dev>
2023-05-18 21:12:08 -0600

    Add support for clblast compute backend

--- build.zig
+++ build.zig
@@ -1,33 +1,74 @@
 const std = @import("std");
 
+const ComputeFramework = enum {
+    accelerate,
+    openblas,
+    cublas,
+    clblast,
+};
+
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const want_lto = b.option(bool, "lto", "Want -fLTO");
+    const compute_framework = b.option(ComputeFramework, "compute-framework", "Specify a library to perform computations (default: none)");
 
-    const lib = b.addStaticLibrary(.{
+    const libggml = b.addStaticLibrary(.{
+        .name = "ggml",
+        .target = target,
+        .optimize = optimize,
+    });
+    libggml.want_lto = want_lto;
+    libggml.linkLibC();
+    libggml.installHeader("ggml.h", "ggml.h");
+    libggml.addCSourceFiles(&.{
+        "ggml.c",
+    }, &.{});
+    if (compute_framework) |framework| {
+        switch (framework) {
+            .accelerate => {
+                libggml.defineCMacro("GGML_USE_ACCELERATE", "1");
+            },
+            .openblas => {
+                libggml.defineCMacro("GGML_USE_OPENBLAS", "1");
+                libggml.linkSystemLibrary("openblas");
+            },
+            .cublas => {
+                libggml.defineCMacro("GGML_USE_CUBLAS", "1");
+            },
+            .clblast => {
+                const clblast = b.dependency("clblast", .{
+                    .target = target,
+                    .optimize = optimize,
+                });
+
+                libggml.addCSourceFile("ggml-opencl.c", &.{});
+                libggml.defineCMacro("GGML_USE_CLBLAST", "1");
+                libggml.linkLibrary(clblast.artifact("clblast"));
+            },
+        }
+    }
+
+    const libllama = b.addStaticLibrary(.{
         .name = "llama",
         .target = target,
         .optimize = optimize,
     });
-    lib.want_lto = want_lto;
-    lib.linkLibCpp();
-    lib.installHeader("llama.h", "llama.h");
-    lib.installHeader("ggml.h", "ggml.h");
-    lib.addCSourceFiles(&.{
-        "ggml.c",
-    }, &.{});
-    lib.addCSourceFiles(&.{
+    libllama.want_lto = want_lto;
+    libllama.linkLibCpp();
+    libllama.installHeader("llama.h", "llama.h");
+    libllama.linkLibrary(libggml);
+    libllama.addCSourceFiles(&.{
         "llama.cpp",
     }, &.{"-std=c++11"});
-    b.installArtifact(lib);
+    b.installArtifact(libllama);
 
     const config_header = b.addConfigHeader(.{}, .{
         .BUILD_NUMBER = std.mem.trim(u8, b.exec(&.{ "git", "rev-list", "--count", "HEAD" }), "\n\r "),
         .BUILD_COMMIT = std.mem.trim(u8, b.exec(&.{ "git", "rev-parse", "--short", "HEAD" }), "\n\r "),
     });
 
-    const build_args = .{ .b = b, .lib = lib, .target = target, .optimize = optimize, .want_lto = want_lto, .config_header = config_header };
+    const build_args = .{ .b = b, .lib = libllama, .target = target, .optimize = optimize, .want_lto = want_lto, .config_header = config_header };
 
     const exe = build_example("main", build_args);
     _ = build_example("quantize", build_args);
